ceebfcfd41b9f3ebc67030ddaa738ecf8359c006
pawximity <chrisloverchio+gis@gmail.com>
2025-08-18 12:30:10 -0600

    progress bar and formatting

--- pawgrate/loader.py
+++ pawgrate/loader.py
@@ -1,36 +1,53 @@
+from getpass import getpass
 import os
 import shutil
 import subprocess
-from getpass import getpass
 
 from pawgrate.config import ImportConfig
 
 
-def load_data(config: ImportConfig) -> tuple[list[str], subprocess.CompletedProcess]:
+def load_data(config):
     if not shutil.which("ogr2ogr"):
-        raise RuntimeError("ogr2ogr not found in PATH. Make sure GDAL is installed")
+        raise RuntimeError(
+            "ogr2ogr not found in PATH. Make sure GDAL is installed")
+    env_copy = os.environ.copy()
     if config.prompt_password:
-        os.environ['PGPASSWORD'] = getpass("Postgres password: ")
+        env_copy['PGPASSWORD'] = getpass("[!] Postgres password: ")
     # specifically using subprocess to avoid the messy dependencies of the bindings...
     command = [
         'ogr2ogr',
-        '-f', 'PostgreSQL',
+        '-f',
+        'PostgreSQL',
         f'PG:host={config.host} dbname={config.dbname} user={config.user} port={config.port}',
         config.src,
-        '-nln', f'{config.schema}.{config.table}',
-        '-lco', f'SCHEMA={config.schema}',
-        '-nlt', config.geomtype,
-        '-lco', 'GEOMETRY_NAME=geom',
-        '-lco', 'FID=gid',
-        '-lco', 'SPATIAL_INDEX=GIST',
-        '-t_srs', f'EPSG:{config.srid}',
+        '-nln',
+        f'{config.schema}.{config.table}',
+        '-lco',
+        f'SCHEMA={config.schema}',
+        '-nlt',
+        config.geomtype,
+        '-lco',
+        'GEOMETRY_NAME=geom',
+        '-lco',
+        'FID=gid',
+        '-lco',
+        'SPATIAL_INDEX=GIST',
+        '-t_srs',
+        f'EPSG:{config.srid}',
     ]
-    if config.mode == 'append':
-        command.append("-append")
-    elif config.mode == 'overwrite':
-        command.append("-overwrite")
+    command.append(write_mode(config.mode))
+    process = subprocess.Popen(command,
+                               env=env_copy,
+                               stdout=subprocess.PIPE,
+                               stderr=subprocess.PIPE,
+                               text=True)
+    return command, process
+
+
+def write_mode(mode):
+    if mode == 'append':
+        return "-append"
+    elif mode == 'overwrite':
+        return "-overwrite"
     else:
-        raise ValueError(f"Invalid mode: {config.mode}")
-    command.append("-progress")
-    result = subprocess.run(command, capture_output=True, text=True)
-    return command, result
+        raise ValueError(f"Invalid mode: {mode}")
